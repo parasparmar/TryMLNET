38b1448436800e1df6ea5fe1dce43878d32638a7
Paras Parmar <parasparmar@gmail.com>
2024-03-21 11:57:02 +0530

    Added in the query utilised instead of the view

---
+++ TryMLNET/Models/Customer.cs
@@ -0,0 +1,9 @@
+﻿namespace TryMLNET.Models
+{
+    public class Customer
+    {
+        public DateTime MonthlySales { get; set; }
+        public string CustomerId { get; set; }
+        public double SaleAmount { get; set; }
+    }
+}
--- TryMLNET/Pages/Index.cshtml.cs
+++ TryMLNET/Pages/Index.cshtml.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Data;
 using System.Data.SqlClient;
 using System.Reflection.PortableExecutable;
+using TryMLNET.Models;
 namespace TryMLNET.Pages
 {
     public class IndexModel : PageModel
@@ -12,21 +13,29 @@ namespace TryMLNET.Pages
 
         public IndexModel(ILogger<IndexModel> logger)
         {
-            _logger = logger;            
+            _logger = logger;
         }
 
         public void OnGet()
         {
-            var cstr = "Server=Mainframe-007;Database=Northwind;Trusted_Connection=True;";            
-            
+            var cstr = "Server=Mainframe-007;Database=Northwind;Trusted_Connection=True;";
+
             using (SqlConnection cn = new SqlConnection(cstr))
             {
                 cn.Open();
-                using (SqlCommand cmd = new SqlCommand("Select * from vwMonthly_Sales_by_Customer", cn))
+                var qry = @"SELECT TOP (100) PERCENT CONVERT(Date, CONVERT(varchar, YEAR(A.OrderDate)) + '-' + CONVERT(varchar, MONTH(A.OrderDate)) + '-' + CONVERT(varchar, 1)) AS MonthlySales
+                            , A.CustomerID
+                            , SUM(B.UnitPrice * B.Quantity - B.Discount) AS SaleAmount
+                            FROM dbo.Orders AS A 
+                            INNER JOIN dbo.[Order Details] AS B ON B.OrderID = A.OrderID
+                            GROUP BY CONVERT(Date, CONVERT(varchar, YEAR(A.OrderDate)) + '-' + CONVERT(varchar, MONTH(A.OrderDate)) + '-' + CONVERT(varchar, 1))
+                            , A.CustomerID
+                            order by CustomerID, MonthlySales";
+                using (SqlCommand cmd = new SqlCommand(qry, cn))
                 {
                     SqlDataReader r = cmd.ExecuteReader();
                     while (r.Read())
-                    {                        
+                    {
                         Customers.Add(new Customer
                         {
                             MonthlySales = ((DateTime)r.GetDateTime(0)).ToLocalTime(),
@@ -39,8 +48,6 @@ namespace TryMLNET.Pages
                     cn.Close();
             }
 
-
-            ////Load sample data
             var sampleData = new MLModel.ModelInput()
             {
                 MonthlySales = DateTime.Parse("01-10-1997 00:00:00"),
@@ -53,9 +60,4 @@ namespace TryMLNET.Pages
         }
     }
 }
-public class Customer
-{
-    public DateTime MonthlySales { get; set; }
-    public string CustomerId { get; set; }
-    public double SaleAmount { get; set; }
-}
+
