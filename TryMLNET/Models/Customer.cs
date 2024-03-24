aecdff3a5b7fab4ee4ef7d0847dd8da8185293fb
Paras Parmar <parasparmar@gmail.com>
2024-03-24 13:39:41 +0530

    AI Outputs to be consumed.

--- TryMLNET/Models/Customer.cs
+++ TryMLNET/Models/Customer.cs
@@ -6,4 +6,11 @@
         public string CustomerId { get; set; }
         public double SaleAmount { get; set; }
     }
+
+    public class PredictionCustomerModel
+    {
+        public DateTime MonthlySales { get; set; }
+        public string CustomerId { get; set; }
+        public double SaleAmount { get; set; }
+    }
 }
--- TryMLNET/Pages/Index.cshtml.cs
+++ TryMLNET/Pages/Index.cshtml.cs
@@ -1,16 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.ML;
+using Models;
 using System.Data;
 using System.Data.SqlClient;
+using System.Drawing;
 using System.Reflection.PortableExecutable;
+using System.Text.Json;
 using TryMLNET.Models;
+using static Plotly.NET.StyleParam;
+using Customer = TryMLNET.Models.Customer;
 namespace TryMLNET.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
         public List<Customer> Customers = new List<Customer>();
-
+        public string MLResult = string.Empty;
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
@@ -28,6 +34,8 @@ namespace TryMLNET.Pages
                             , SUM(B.UnitPrice * B.Quantity - B.Discount) AS SaleAmount
                             FROM dbo.Orders AS A 
                             INNER JOIN dbo.[Order Details] AS B ON B.OrderID = A.OrderID
+                            WHERE 1=1
+                            And CustomerID = 'ALFKI'
                             GROUP BY CONVERT(Date, CONVERT(varchar, YEAR(A.OrderDate)) + '-' + CONVERT(varchar, MONTH(A.OrderDate)) + '-' + CONVERT(varchar, 1))
                             , A.CustomerID
                             order by CustomerID, MonthlySales";
@@ -48,14 +56,30 @@ namespace TryMLNET.Pages
                     cn.Close();
             }
 
-            var sampleData = new MLModel.ModelInput()
-            {
+            MLContext mlContext = new MLContext();
+
+            IDataView trainingData = mlContext.Data.LoadFromEnumerable(Customers);
+
+            // 2. Specify data preparation and model training pipeline
+            var pipeline = mlContext.Transforms.Concatenate("Features", new[] { "MonthlySales" })
+                .Append(mlContext.Regression.Trainers.FastForest());
+            
+
+            // 3. Train model
+            var model = pipeline.Fit(trainingData);
+
+
+            // 4. Make a prediction
+            var monthlySales = new Customer {
                 MonthlySales = DateTime.Parse("01-10-1997 00:00:00"),
-                CustomerID = @"ALFKI",
+                CustomerId = @"ALFKI"
             };
+            var saleAmount = mlContext.Model.CreatePredictionEngine<Customer, PredictionCustomerModel>(model).Predict(monthlySales);
+
 
             //Load model and predict output
-            var result = MLModel.Predict(sampleData);
+
+            MLResult = $"Predicted Monthly Sales for {monthlySales.CustomerId}: {monthlySales.MonthlySales.ToLongDateString} sq ft= {saleAmount.SaleAmount:C}k"; // JsonSerializer.Serialize(saleAmount);
 
         }
     }
